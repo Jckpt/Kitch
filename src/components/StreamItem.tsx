aae09a03c678ee1abea2b64339a7f3ee9ac07938
jckpt <krawcu333@gmail.com>
2023-09-21 16:43:35 +0200

    fix stream thumbnail aspect ratio

--- src/components/StreamItem.tsx
+++ src/components/StreamItem.tsx
@@ -13,16 +13,16 @@ const StreamItem = ({
       href={`https://twitch.tv/${user_login}`}
       target="_blank"
       rel="noopener noreferrer"
-      className="bg-neutral-900 items-center flex justify-start pl-0 p-2 hover:bg-neutral-800">
-      <div className="h-[54px] w-[96px] flex items-center">
+      className="bg-neutral-900 items-center flex justify-start p-2 hover:bg-neutral-800">
+      <div className="h-[54px] w-[96px] flex items-center rounded-sm">
         <img
           src={`https://static-cdn.jtvnw.net/previews-ttv/live_user_${user_login}-96x54.jpg`}
           alt="stream preview"
-          className={`h-fit rounded-md`}
+          className="h-[54px] w-[96px] max-w-none rounded-sm"
           style={{ display: loaded ? "block" : "none" }}
           onLoad={() => setLoaded(true)}
         />
-        {!loaded && <Skeleton className={`h-full w-full bg-neutral-800`} />}
+        {!loaded && <Skeleton className={`h-[54px] w-[96px] bg-neutral-700`} />}
       </div>
       <div className="flex w-full flex-col pl-1.5">
         <div className="flex justify-between">
