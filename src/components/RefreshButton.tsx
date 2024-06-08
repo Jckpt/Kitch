fb56bddf8e64138acc2961db65c85e5770eb6119
jckpt <krawcu333@gmail.com>
2024-06-08 13:59:06 +0200

    disable refresh button when on wrong tab

---
+++ src/components/RefreshButton.tsx
@@ -0,0 +1,42 @@
+import { IconRefresh } from "@tabler/icons-react"
+import { useAtom } from "jotai"
+import React, { useState } from "react"
+
+import { currentTabAtom } from "~src/lib/util"
+import { sendRuntimeMessage } from "~src/lib/util/helperFunc"
+
+const RefreshButton = () => {
+  const [isRefreshing, setIsRefreshing] = useState(false)
+  const [currentTab] = useAtom(currentTabAtom)
+  const handleRefresh = () => {
+    // disable button if already refreshing
+    if (isRefreshing) return
+
+    setIsRefreshing(true)
+    sendRuntimeMessage("refresh")
+    setTimeout(() => {
+      setIsRefreshing(false)
+    }, 1000)
+  }
+  if (currentTab === "followed") {
+    return (
+      <>
+        <IconRefresh
+          className={`transition-all ease-in-out duration-300 hover:cursor-pointer opacity-75 hover:opacity-100 
+      ${isRefreshing ? "animate-[spin_1s_linear_1]" : ""}
+      `}
+          onClick={handleRefresh}
+        />
+      </>
+    )
+  }
+  return (
+    <>
+      <IconRefresh
+        className={`transition-all ease-in-out duration-300 opacity-20 ${isRefreshing ? "animate-[spin_1s_linear_1]" : ""} `}
+      />
+    </>
+  )
+}
+
+export default RefreshButton
--- src/components/tabs/TopBar.tsx
+++ src/components/tabs/TopBar.tsx
@@ -1,28 +1,18 @@
 import { useAutoAnimate } from "@formkit/auto-animate/react"
-import { IconRefresh } from "@tabler/icons-react"
 import { useAtom } from "jotai"
-import React, { useState } from "react"
+import React from "react"
 
 import { searchQueryAtom } from "~src/lib/util"
-import { sendRuntimeMessage } from "~src/lib/util/helperFunc"
 
 import PlatformIcon from "../PlatformIcon"
+import RefreshButton from "../RefreshButton"
 import { Input } from "../ui/input"
 
 const TopBar = ({ twitchLoggedIn }) => {
   const [searchQuery, setSearchQuery] = useAtom(searchQueryAtom)
-  const [isRefreshing, setIsRefreshing] = useState(false)
-  const [parent] = useAutoAnimate({ duration: 300 })
-  const handleRefresh = () => {
-    // disable button if already refreshing
-    if (isRefreshing) return
 
-    setIsRefreshing(true)
-    sendRuntimeMessage("refresh")
-    setTimeout(() => {
-      setIsRefreshing(false)
-    }, 1000)
-  }
+  const [parent] = useAutoAnimate({ duration: 300 })
+
   return (
     <div
       className="p-2 h-12 flex-grow-0 flex-shrink flex justify-between gap-2 items-center bg-zinc-900"
@@ -36,12 +26,7 @@ const TopBar = ({ twitchLoggedIn }) => {
         disabled={!twitchLoggedIn}
         onChange={(e) => setSearchQuery(e.target.value)}
       />
-      <IconRefresh
-        className={`hover:cursor-pointer opacity-75 hover:opacity-100 
-      ${isRefreshing ? "animate-[spin_1s_linear_1]" : ""}
-      `}
-        onClick={handleRefresh}
-      />
+      <RefreshButton />
     </div>
   )
 }
