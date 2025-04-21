75e327b76d7c5ee815a6bb3225b51ea25a98aee6
jckpt <krawcu333@gmail.com>
2025-04-21 21:59:08 +0200

    add render

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { IconBrandChrome, IconBrandFirefox } from "@tabler/icons-react"
 import { useEffect, useState } from "react"
+
 import "./App.css"
 
 const Authentication = ({ hasToken }: { hasToken: boolean }) => (
@@ -31,25 +32,34 @@ const Boarding = () => (
       <div className="max-w-[300px]">
         <h1 className="text-4xl font-bold text-white mb-2">Kitch</h1>
         <p className="text-[#a1a1a1] text-lg">
-          Follow your favorite streamers<br />
-          on <span className="text-[#53fc19]">Kick</span> and <span className="text-[#a970ff]">Twitch</span>
+          Follow your favorite streamers
+          <br />
+          on <span className="text-[#53fc19]">Kick</span> and{" "}
+          <span className="text-[#a970ff]">Twitch</span>
         </p>
       </div>
       <div className="space-y-4 max-w-[300px]">
-        <a href="https://chromewebstore.google.com/detail/kitch-kick-twitch-notific/afinpfknmmcbkmbgjcoljffonbmkccnl?hl=en" 
-           className="flex items-center space-x-3 text-white bg-[#242424] hover:bg-[#2f2f2f] transition-all rounded-lg px-6 py-3 w-full border border-[#333333] hover:border-[#404040] shadow-lg hover:shadow-xl">
+        <a
+          href="https://chromewebstore.google.com/detail/kitch-kick-twitch-notific/afinpfknmmcbkmbgjcoljffonbmkccnl?hl=en"
+          className="flex items-center space-x-3 text-white bg-[#242424] hover:bg-[#2f2f2f] transition-all rounded-lg px-6 py-3 w-full border border-[#333333] hover:border-[#404040] shadow-lg hover:shadow-xl">
           <IconBrandChrome size={24} />
           <span>Add to Chrome</span>
         </a>
-        <a href="https://addons.mozilla.org/en-US/firefox/addon/kitch/" 
-           className="flex items-center space-x-3 text-white bg-[#242424] hover:bg-[#2f2f2f] transition-all rounded-lg px-6 py-3 w-full border border-[#333333] hover:border-[#404040] shadow-lg hover:shadow-xl">
+        <a
+          href="https://addons.mozilla.org/en-US/firefox/addon/kitch/"
+          className="flex items-center space-x-3 text-white bg-[#242424] hover:bg-[#2f2f2f] transition-all rounded-lg px-6 py-3 w-full border border-[#333333] hover:border-[#404040] shadow-lg hover:shadow-xl">
           <IconBrandFirefox size={24} />
           <span>Add to Firefox</span>
         </a>
       </div>
     </div>
-    <div className="hidden md:block bg-[#1f1f1f]/30 rounded-2xl aspect-square w-full max-w-md mx-auto">
-      {/* Placeholder for image */}
+    <div className="hidden md:flex rounded-2xl aspect-square w-full max-w-md mx-auto justify-center items-center">
+      <img
+        src="/render.png"
+        alt="Kitch"
+        draggable="false"
+        className="w-3/4 h-3/4 mb-14"
+      />
     </div>
   </div>
 )
@@ -69,7 +79,8 @@ function App() {
   return (
     <div className="overscroll-none min-h-screen w-screen bg-gradient-to-br from-[#181818] via-[#1f1f1f] to-[#242424]">
       <div className="container mx-auto px-4 h-screen flex flex-col items-center justify-center relative">
-        <div className={`w-full max-w-5xl ${isAuthenticated || hasToken ? 'max-w-md' : ''}`}>
+        <div
+          className={`w-full max-w-5xl ${isAuthenticated || hasToken ? "max-w-md" : ""}`}>
           {isAuthenticated || hasToken ? (
             <div className="space-y-8 flex flex-col items-center">
               <Authentication hasToken={isAuthenticated} />
@@ -78,12 +89,11 @@ function App() {
             <Boarding />
           )}
         </div>
-        <a 
-          href="https://github.com/jckpt" 
-          target="_blank" 
-          rel="noopener noreferrer" 
-          className="absolute bottom-4 text-[#a1a1a1] hover:text-white transition-colors text-sm"
-        >
+        <a
+          href="https://github.com/jckpt"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="absolute bottom-4 text-[#a1a1a1] hover:text-white transition-colors text-sm">
           made by me
         </a>
       </div>
